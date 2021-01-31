678693e44fdfd21b948bf17d0102e84cdc14fc88
DanielDC95 <ddc95@live.com>
2021-01-31 02:41:13 -0500

    The methods select, insert and update were created in the DataBase class.

--- RouletteAPI/DataBase/DataBase.cs
+++ RouletteAPI/DataBase/DataBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
 using Npgsql;
 
 namespace RouletteAPI.DataBase
@@ -9,31 +10,286 @@ namespace RouletteAPI.DataBase
     public class DataBase
     {
         private static NpgsqlConnection dataBaseConnection = new NpgsqlConnection("Server = localhost; User Id = postgres; Password = 1234; DataBase = roulette_db");
+        private static bool connected = false;
 
-        public static bool connect()
+        public struct DataToQuerySelect
+        {
+            public string tableName;
+            public List<string> fields;
+            public List<FieldsToOrder> orderBy;
+        }
+
+        public struct DataToQueryInsert
+        {
+            public string tableName;
+            public List<FieldAndValue> fields;
+        }
+
+        public struct DataToQueryUpdate
+        {
+            public string tableName;
+            public int id;
+            public List<FieldAndValue> fields;
+        }
+
+        public struct FieldAndValue
+        {
+            public string filedName;
+            public string value;
+        }
+
+        public struct FieldsToOrder
+        {
+            public string filedName;
+            public string orderType;
+        }
+
+        public static void connect()
         {
             try
             {
-                dataBaseConnection.Open();
-                return true;
+                if(connected == false)
+                {
+                    dataBaseConnection.Open();
+                    connected = true;
+                }
             }
             catch
             {
-                return false;
+                connected = false;
             }
         }
 
-        public static bool desconnect()
+        public static void desconnect()
         {
             try
             {
-                dataBaseConnection.Close();
-                return true;
+                if(connected == true)
+                {
+                    dataBaseConnection.Close();
+                    connected = false;
+                }
             }
             catch
             {
-                return false;
+                connected = true;
             }
+        }
+
+        public static bool isConnected()
+        {
+            return connected;
+        }
+
+        public static DataTable select(DataToQuerySelect queryData)
+        {
+            DataTable table = new DataTable();
+            string fields = getFieldsForSelect(queryData);
+            string orderBy = getOrderBy(queryData);
+            string query = "Select " + fields + " From " + queryData.tableName + " Order By " + orderBy + ";";
+            try
+            {
+                if (connected == true)
+                {
+                    NpgsqlCommand command = new NpgsqlCommand(query, dataBaseConnection);
+                    NpgsqlDataAdapter data = new NpgsqlDataAdapter(command);
+                    data.Fill(table);
+                }
+                else
+                {
+                    table = null;
+                }
+            }
+            catch (Exception)
+            {
+                table = null;
+            }
+            
+            return table;
+        }
+
+        public static bool insert(DataToQueryInsert queryData)
+        {
+            bool insertResult;
+            int rowsAffected = 0;
+            DataTable table = new DataTable();
+            string fields = getFieldsForInsert(queryData);
+            string values = getValuesForInsert(queryData);
+            string query = "Insert Into " + queryData.tableName +  "(" + fields + ") values (" + values + ");";
+            try
+            {
+                if (connected == true)
+                {
+                    NpgsqlCommand command = new NpgsqlCommand(query, dataBaseConnection);
+                    rowsAffected = command.ExecuteNonQuery();
+                    if(rowsAffected > 0)
+                    {
+                        insertResult = true;
+                    }
+                    else
+                    {
+                        insertResult = false;
+                    }
+                }
+                else
+                {
+                    insertResult = false;
+                }
+
+            }
+            catch (Exception)
+            {
+                insertResult = false;
+            }
+
+            return insertResult;
+        }
+
+        public static bool update(DataToQueryUpdate queryData)
+        {
+            bool updateResult;
+            int rowsAffected = 0;
+            DataTable table = new DataTable();
+            string fields = getFieldsForUpdate(queryData);
+            string query = "Update " + queryData.tableName + " set " + fields + " Where id = " + queryData.id + " ;";
+            try
+            {
+                if (connected == true)
+                {
+                    NpgsqlCommand command = new NpgsqlCommand(query, dataBaseConnection);
+                    rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        updateResult = true;
+                    }
+                    else
+                    {
+                        updateResult = false;
+                    }
+                }
+                else
+                {
+                    updateResult = false;
+                }
+
+            }
+            catch (Exception)
+            {
+                updateResult = false;
+            }
+
+            return updateResult;
+        }
+
+        private static string getFieldsForSelect(DataToQuerySelect queryData)
+        {
+            string fields = "";
+            foreach(string field in queryData.fields)
+            {
+                if (String.IsNullOrEmpty(fields))
+                {
+                    fields = field;
+                }
+                else
+                {
+                    fields = fields + ", " + field;
+                }
+            }
+
+            return fields;
+        }
+
+        private static string getOrderBy(DataToQuerySelect queryData)
+        {
+            string orderBy = "";
+            foreach (FieldsToOrder field in queryData.orderBy)
+            {
+                if (String.IsNullOrEmpty(orderBy))
+                {
+                    orderBy = field.filedName + " " + field.orderType;
+                }
+                else
+                {
+                    orderBy = orderBy + ", " + field.filedName + " " + field.orderType; ;
+                }
+            }
+
+            return orderBy;
+        }
+
+        private static string getFieldsForInsert(DataToQueryInsert queryData)
+        {
+            string fields = null;
+            try
+            {
+                foreach (FieldAndValue field in queryData.fields)
+                {
+                    if (String.IsNullOrEmpty(fields))
+                    {
+                        fields = field.filedName;
+                    }
+                    else
+                    {
+                        fields = fields + ", " + field.filedName;
+                    }
+                }
+            }
+            catch
+            {
+                fields = "";
+            }
+            
+            return fields;
+        }
+
+        private static string getFieldsForUpdate(DataToQueryUpdate queryData)
+        {
+            string fields = null;
+            try
+            {
+                foreach (FieldAndValue field in queryData.fields)
+                {
+                    if (String.IsNullOrEmpty(fields))
+                    {
+                        fields = field.filedName + " = '" + field.value + "'";
+                    }
+                    else
+                    {
+                        fields = fields + ", " + field.filedName + " = '" + field.value + "'";
+                    }
+                }
+            }
+            catch
+            {
+                fields = "";
+            }
+
+            return fields;
+        }
+
+        private static string getValuesForInsert(DataToQueryInsert queryData)
+        {
+            string values = null;
+            try
+            {
+                foreach (FieldAndValue field in queryData.fields)
+                {
+                    if (String.IsNullOrEmpty(values))
+                    {
+                        values = "'" + field.value + "'";
+                    }
+                    else
+                    {
+                        values = values + ", '" + field.value + "'";
+                    }
+                }
+            }
+            catch
+            {
+                values = "";
+            }
+            
+            return values;
         }
     }
 }
--- UnitTestRouletteAPI/DataBase_UnitTest.cs
+++ UnitTestRouletteAPI/DataBase_UnitTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RouletteAPI.DataBase;
 
 namespace UnitTestRouletteAPI
@@ -9,15 +10,118 @@ namespace UnitTestRouletteAPI
         [TestMethod]
         public void TestConnect()
         {
-            var connectResult = DataBase.connect();
+            DataBase.connect();
+            var connectResult = DataBase.isConnected();
             Assert.IsTrue(connectResult);
         }
 
         [TestMethod]
         public void TestDesonnect()
         {
-            var connectResult = DataBase.desconnect();
-            Assert.IsTrue(connectResult);
+            DataBase.desconnect();
+            var connectResult = DataBase.isConnected();
+            Assert.IsFalse(connectResult);
+        }
+
+        [TestMethod]
+        public void TestSelect()
+        {
+            DataBase.connect();
+            List<string> fields = new List<string>();
+            
+            DataBase.DataToQuerySelect data = new DataBase.DataToQuerySelect();
+            fields.Add("id");
+            fields.Add("state");
+            data.tableName = "states";
+            data.fields = fields;
+            DataBase.FieldsToOrder field = new DataBase.FieldsToOrder();
+            List<DataBase.FieldsToOrder> orderBy = new List<DataBase.FieldsToOrder>();
+            field.filedName = "id";
+            field.orderType = "desc";
+            orderBy.Add(field);
+            data.orderBy = orderBy;
+            var tableResult = DataBase.select(data);
+            DataBase.desconnect();
+            Assert.IsNotNull(tableResult);
+        }
+
+        [TestMethod]
+        public void TestInsert_Ok()
+        {
+            DataBase.connect();
+            DataBase.FieldAndValue field = new DataBase.FieldAndValue();
+            List <DataBase.FieldAndValue> fields = new List<DataBase.FieldAndValue>();
+            DataBase.DataToQueryInsert data = new DataBase.DataToQueryInsert();
+            field.filedName = "last_number";
+            field.value = "35";
+            fields.Add(field);
+            field.filedName = "state_id";
+            field.value = "2";
+            fields.Add(field);
+            data.tableName = "roulettes";
+            data.fields = fields;
+            var insertResult = DataBase.insert(data);
+            DataBase.desconnect();
+            Assert.IsTrue(insertResult);
+        }
+
+        [TestMethod]
+        public void TestInsertWithoutFieldsOrValues()
+        {
+            DataBase.connect();
+            DataBase.FieldAndValue field = new DataBase.FieldAndValue();
+            List<DataBase.FieldAndValue> fields = new List<DataBase.FieldAndValue>();
+            DataBase.DataToQueryInsert data = new DataBase.DataToQueryInsert();
+            field.filedName = null;
+            field.value = null;
+            fields.Add(field);
+            data.tableName = "roulettes";
+            data.fields = fields;
+            var insertResult = DataBase.insert(data);
+            DataBase.desconnect();
+            Assert.IsFalse(insertResult);
+        }
+
+        [TestMethod]
+        public void TestUpdate_Ok()
+        {
+            DataBase.connect();
+            DataBase.FieldAndValue field = new DataBase.FieldAndValue();
+            List<DataBase.FieldAndValue> fields = new List<DataBase.FieldAndValue>();
+            DataBase.DataToQueryUpdate data = new DataBase.DataToQueryUpdate();
+            field.filedName = "last_number";
+            field.value = "36";
+            fields.Add(field);
+            field.filedName = "state_id";
+            field.value = "1";
+            fields.Add(field);
+            data.tableName = "roulettes";
+            data.id = 1;
+            data.fields = fields;
+            var updateResult = DataBase.update(data);
+            DataBase.desconnect();
+            Assert.IsTrue(updateResult);
+        }
+
+        [TestMethod]
+        public void TestUpdateWithoutTable()
+        {
+            DataBase.connect();
+            DataBase.FieldAndValue field = new DataBase.FieldAndValue();
+            List<DataBase.FieldAndValue> fields = new List<DataBase.FieldAndValue>();
+            DataBase.DataToQueryUpdate data = new DataBase.DataToQueryUpdate();
+            field.filedName = "last_number";
+            field.value = "36";
+            fields.Add(field);
+            field.filedName = "state_id";
+            field.value = "2";
+            fields.Add(field);
+            data.tableName = "";
+            data.id = 1;
+            data.fields = fields;
+            var updateResult = DataBase.update(data);
+            DataBase.desconnect();
+            Assert.IsFalse(updateResult);
         }
     }
 }
